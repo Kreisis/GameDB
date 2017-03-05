edc2f99b2119d9fc2b8b94db30acbc1a1997a02a
Kreisis <kreisis4@gmail.com>
2017-03-05 17:40:38 +0200

    Sutvarkytas HomePage interface, javascript krauna visas nuotraukas

--- Prototipas/js/Sample.js
+++ Prototipas/js/Sample.js
@@ -1,18 +1,48 @@
 ﻿$(document).ready(function () {
+    fixImageDimensions();
     init();
 });
-var list = [{ "id": "side-pic-a", "gameId": 20964 }, { "id": "side-pic-b", "gameId": 4725 }];
 
 function init() {
     list.forEach(function (item) {
-        callToServer(item.gameId)
+        callToServer(item.gameId[0]);
     });
 }
 
+var list = [{ "id": "side-pic-a", "gameId": [20964], "type": "tall" , "idIndex": -1},
+    { "id": "side-pic-b", "gameId": [4725], "type": "tall", "idIndex": -1 },
+    { "id": "pic-a-1", "gameId": [20964], "type": "wide", "idIndex": -1 },
+    { "id": "pic-a-2", "gameId": [4725], "type": "wide", "idIndex": -1 },
+    { "id": "pic-b-1", "gameId": [4725], "type": "square", "idIndex": -1 },
+    { "id": "pic-b-2", "gameId": [4725], "type": "square", "idIndex": -1 },
+    { "id": "bottom-pic-1", "gameId": [4725], "type": "wide", "idIndex": -1 },
+    { "id": "bottom-pic-2", "gameId": [4725], "type": "wide", "idIndex": -1 },
+    { "id": "bottom-pic-3", "gameId": [4725], "type": "wide", "idIndex": -1 }];
+
+function fixImageDimensions() {
+    $(".a-pic").css("height", $(".b-pic").height());
+    $(".side-pic").css("height", ($(".a-pic").outerHeight(true) + $(".a-pic").height()));
+    $(".bottom-pic").css("height", $(".bottom-pic-mid").height());
+    /*$(".game-name-text").css("line-height", $(".game-name-text").height());*/
+}
+
 function gamer(data) {
+    console.log(data);
     list.forEach(function (item) {
-        if (item.gameId === data.results.id) {
-            (document).getElementsByClassName(item.id)[0].src = data.results.image.medium_url;
+        if (item.gameId[0] === data.results.id) {
+            if (item.type == "wide") {
+                var height = $("#" + item.id).height();
+                /*(document).getElementsByClassName(item.id)[0].src = data.results.image.medium_url;*/
+                $("#" + item.id).attr('src', data.results.image.screen_url);
+                $("#" + item.id).css("height", height);
+            }
+            else {
+                var height = $("#" + item.id).height();
+                /*(document).getElementsByClassName(item.id)[0].src = data.results.image.medium_url;*/
+                $("#" + item.id).attr('src', data.results.image.medium_url);
+                $("#" + item.id).css("height", height);
+            }
+                      
         }
     });
 }
@@ -21,7 +51,9 @@ function callToServer(gameId) {
     $.ajax({
         url: "http://www.giantbomb.com/api/game/3030-" + gameId + "/?api_key=739777161fa7c039190e538d0715c9671c146cb1&format=jsonp&field_list=image,id",
         type: "get",
-        data: { json_callback: "gamer"},
+        data: { json_callback: "gamer" },
         dataType: "jsonp"
     });
 }
+
+
